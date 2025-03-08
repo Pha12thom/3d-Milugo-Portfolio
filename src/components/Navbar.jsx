e169641b14f6dc37c49de1c00a7c4ff3535ea671
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2025-03-08 03:03:12 +0300

    add terminal 3d

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React,{useEffect, useState} from "react"
+import {Link} from "react-router-dom"
 
+import {styles} from "../styles"
+import {newLinks} from "../constants"
+import {Logo, menu,close } from "../assets"
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.padding} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+
+      <div className="w-full flex justify-between items-center max-w-7x1 mx-auto"
+    ></div>
+    </nav>
   )
 }
 
